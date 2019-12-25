a4abfb60ac4e01908a5bfe99435644e2d1301402
giteshraheja <thundergitesh@gmail.com>
2019-12-25 23:04:02 +0530

    Added froala_editor

--- blog/models.py
+++ blog/models.py
@@ -2,11 +2,12 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from froala_editor.fields import FroalaField
 
 
 class Post(models.Model):
     title = models.CharField(max_length=500)
-    content = models.TextField()
+    content = FroalaField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     # image = models.ImageField(upload_to='images')
--- django_project/urls.py
+++ django_project/urls.py
@@ -47,6 +47,7 @@ urlpatterns = [
          ),
          name='password_reset_complete'),
     path('', include('blog.urls')),
+    # path('froala_editor/', include('froala_editor.urls')), # for froala image upload only
 ]
 
 
